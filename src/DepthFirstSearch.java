58a02682b5bc870b326e1ec618ed84532a7d6bd7
techarenz <adam.ewing.nz@gmail.com>
2023-02-12 19:40:35 +0800

    Initial commit

---
+++ src/DepthFirstSearch.java
@@ -0,0 +1,42 @@
+
+public class DepthFirstSearch {
+
+
+    public static void main(String[] args) {
+        // Declare and initialize your adjacency list here.
+
+        // Call your DFS method here.
+
+    }
+
+    public static void dfsIterative(int[][] adjacencyList) {
+
+        // Create a new stack.
+
+        // Create an array that will record whether we have visited each node.
+
+        // Push the starting node onto the stack.
+
+        // While our stack is not empty ...
+
+            // Pop the top node from the stack.
+
+            // Mark this node as visited.
+
+            // Print the node - not fundamental to our algorithm - just so we can see the path that our algorithm takes.
+
+            // Get the neighbours of this node.
+
+            // For each neighbouring node ...
+
+                // Obtain a reference to the neighbour node.
+
+                // If it has not yet been visited, mark it as visited and push it onto the stack.
+
+    }
+
+    public static void dfsRecursive(int[][] graph, boolean[] visited, int currentNode) {
+        
+    }
+
+}
